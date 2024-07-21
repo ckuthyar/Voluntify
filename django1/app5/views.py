eb7e40b7321b6e6b2d837565fa1c10ff590da573
ckuthyar <ckuthyar@gmail.com>
2024-07-22 05:13:32 +0530

    Mathematical tables <br> not giving new line

--- django1/app5/views.py
+++ django1/app5/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 def home(request):
-    return render(request,'app5/index.html',{'param1':"Mathematical Tables"})
+    result=tables1(3,6)
+    return render(request,'app5/index.html',{'param1':result})
 
-# Create your views here.
+def tables1(start,end):
+    list1=[]
+    for j in range(start,end+1,1):
+        for i in range(1,11,1):
+            info1=str(j)+"*"+str(i)+"="+str(j*i)
+            list1.append(info1)
+        list1.append('<br>')
+    return list1

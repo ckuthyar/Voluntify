8b1a04649d3f69b8b3b7a6effcf42e43aed0fe5b
ckuthyar <ckuthyar@gmail.com>
2024-07-22 08:04:49 +0530

    Mathematical tables with border for each table

---
+++ django1/app10/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+def home(request):
+    result=tables2(50,54)
+    return render(request,'app10/index.html',{'param1':result})
+
+def tables2(start,end):
+    list2=[]
+    for j in range(start,end+1,1):
+        list1=[]
+        for i in range(1,11,1):
+            info1=str(j)+"*"+str(i)+"="+str(j*i)
+            list1.append(info1)
+        list2.append(list1)
+    return list2
--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'app10',
 	'app9',
 	'app8',
     'app1',
